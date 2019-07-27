16f200dc8c60e1f5e2ca6a4e50d1d56294dcfe22
Franz Roleda <franz.roleda@protonmail.com>
2019-07-27 10:32:05 +0800

    To update input field on back and forward button

--- src/app/modules/search/components/search-input/search-input.component.ts
+++ src/app/modules/search/components/search-input/search-input.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-search-input',
   templateUrl: './search-input.component.html',
   styleUrls: ['./search-input.component.scss']
 })
-export class SearchInputComponent implements OnInit {
+export class SearchInputComponent implements OnInit, OnChanges {
   searchCountryQuery = '';
   @Input() query: string;
   ngOnInit(): void {
     if (this.query) {
       this.searchCountryQuery = this.query;
     }
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.searchCountryQuery = changes.query.currentValue;
   }
 }
